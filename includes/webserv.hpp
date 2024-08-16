3983b2140e79d0abc9b4d1a1dd3f926a53321903
Okan KOCA <neazenmeh@gmail.com>
2024-08-16 16:48:01 +0200

    receiving and sending data, not good enough as of yet

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 11:53:26 by okoca             #+#    #+#             */
-/*   Updated: 2024/08/16 15:38:50 by okoca            ###   ########.fr       */
+/*   Updated: 2024/08/16 15:46:56 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 #include <sys/socket.h>
 #include <signal.h>
 #include <netinet/in.h>
+#include <cstring>
 
 extern bool	end_sig;
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 11:53:09 by okoca             #+#    #+#             */
-/*   Updated: 2024/08/16 15:41:31 by okoca            ###   ########.fr       */
+/*   Updated: 2024/08/16 16:44:56 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <webserv.hpp>
+
+# define BUF_SIZE 1024
 
 int	port = 8080;
 
@@ -22,9 +24,10 @@ bool	end_sig = false;
 void	handle_sigint(int sig)
 {
 	(void)sig;
-	end_sig = true;
-	if (server_fd != -1)
-		close(server_fd);
+	throw std::runtime_error("exiting...");
+	// end_sig = true;
+	// if (server_fd != -1)
+	// 	close(server_fd);
 }
 
 int main()
@@ -42,18 +45,37 @@ int main()
 	std::cout << "[" << server_fd << "]" << " listening on port: " << port << std::endl;
 	listen(server_fd, 5);
 
-	while (1)
+	try
 	{
-		client_fd = accept(server_fd, NULL, NULL);
-		if (end_sig)
-			break;
-		std::cout << "received signal by connection: [" << client_fd << "]" << std::endl;
+		while (!end_sig)
+		{
+			client_fd = accept(server_fd, NULL, NULL);
+			std::cout << "received signal by connection: [" << client_fd << "]" << std::endl;
 
-		std::string	buf_s;
-		char	buf[1024] = {0};
+			std::string	buf_s;
+			ssize_t total_bytes = 0;
+			// while (1)
+			// {
+				char	buf[BUF_SIZE] = {0};
 
-		recv(client_fd, buf, sizeof(buf), 0);
-		std::cout << "Message: " << std::endl << buf << std::endl;
+				ssize_t b_read = recv(client_fd, buf, BUF_SIZE - 1, 0);
+				if (b_read <= 0)
+					break;
+				buf[b_read] = '\0';
+				buf_s += buf;
+				total_bytes += b_read;
+			// }
+			std::cout << "CONTENT: " << std::endl << buf_s << std::endl;
+			std::cout << "bytes read: " << total_bytes << std::endl;
+			char hello[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContentLength: 12\n\nHello world!";
+			send(client_fd, hello, strlen(hello), 0);
+			close(client_fd);
+		}
+	}
+
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
 	}
 	if (server_fd != -1)
 		close(server_fd);
