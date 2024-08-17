40c82846b74f22a9ffab6f3a8871ac57bd3fd6ca
Okan KOCA <neazenmeh@gmail.com>
2024-08-17 12:20:02 +0200

    added colors to my logging for better visibility

--- src/log.cpp
+++ src/log.cpp
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 11:12:19 by okoca             #+#    #+#             */
-/*   Updated: 2024/08/17 12:06:40 by okoca            ###   ########.fr       */
+/*   Updated: 2024/08/17 12:18:23 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ void	log(const std::string &s)
 
 void	log(const std::string &s, const Server &serv)
 {
-	std::cout << log_data(s, serv);
+	std::cout << Color::set_color(Color::FG_GREEN) << log_data(s, serv) << Color::reset();
 }
 
 void	log_err(const std::string &s)
 {
-	std::cerr << log_data(s);
+	std::cerr << Color::set_color(Color::FG_RED) << log_data(s) << Color::reset();
 }
 
 void	log_err(const std::string &s, const Server &serv)
 {
-	std::cerr << log_data(s, serv);
+	std::cerr << Color::set_color(Color::FG_RED) << log_data(s, serv) << Color::reset();
 }
 
 inline static std::string	log_data(const std::string &s)
