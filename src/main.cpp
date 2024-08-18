595e22dd643f78e5e93d1ea48aae5e8f5245960e
Okan KOCA <neazenmeh@gmail.com>
2024-08-18 17:10:52 +0200

    now can listen on multiple server at once with epoll, still need to read the data.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 11:53:09 by okoca             #+#    #+#             */
-/*   Updated: 2024/08/18 15:23:19 by okoca            ###   ########.fr       */
+/*   Updated: 2024/08/18 17:10:00 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,38 @@ void	handle_sigint(int sig)
 	throw std::runtime_error("exiting...");
 }
 
-# define MAX_EPOLL_EVENTS 16
+# define MAX_SERVERS 16
 
 
-// Server	serv("0.0.0.0", 8080);
-
-// serv.start_server();
-// serv.connect();
 int main()
 {
+	std::vector<Server*> servers;
+	int	epoll_instance = -1;
 	try
 	{
 		signal(SIGINT, handle_sigint);
 
-		int	epoll_instance = -1;
 		int	event_ready = 0;
-		std::vector<Server> servers;
-		servers.push_back(Server("0.0.0.0", 8080));
-		servers.push_back(Server("0.0.0.0", 8081));
+		servers.push_back(new Server("0.0.0.0", 8080));
+		servers.push_back(new Server("0.0.0.0", 8081));
+		servers.push_back(new Server("0.0.0.0", 8082));
+		servers.push_back(new Server("0.0.0.0", 8083));
+		servers.push_back(new Server("0.0.0.0", 8084));
 
 		std::vector<epoll_event>	e_servers;
 		// std::vector<epoll_event>	e_queue;
-		epoll_event	e_queue[MAX_EPOLL_EVENTS];
+		epoll_event	e_queue[MAX_SERVERS];
 
 		epoll_instance = epoll_create1(0);
 		if (epoll_instance < 0)
 			throw std::runtime_error("Failed to create epoll instance");
 
 
-		std::vector<Server>::iterator	it;
+		std::vector<Server*>::iterator	it;
 		for (it = servers.begin(); it != servers.end(); it++)
 		{
-			SOCKET	socket_fd = (*it).get_socket_fd();
-			(*it).start_server();
+			SOCKET	socket_fd = (*(*it)).get_socket_fd();
+			(*(*it)).start_server();
 			epoll_event	event;
 			event.events = EPOLLIN;
 			event.data.fd = socket_fd;
@@ -64,10 +63,9 @@ int main()
 			}
 		}
 
-
 		while (true)
 		{
-			event_ready = epoll_wait(epoll_instance, e_queue, MAX_EPOLL_EVENTS, -1);
+			event_ready = epoll_wait(epoll_instance, e_queue, MAX_SERVERS, -1);
 			if (event_ready < 0)
 				throw std::runtime_error("Failed on epoll_wait");
 
@@ -82,73 +80,38 @@ int main()
 	{
 		std::cerr << e.what() << '\n';
 	}
+
+	std::vector<Server*>::iterator it;
+	for (it = servers.begin(); it != servers.end(); it++)
+		delete (*it);
+	if (epoll_instance != -1)
+		close(epoll_instance);
 }
-
-// # define BUF_SIZE 1024
-
-// int	port = 8080;
-
-// int	server_fd = -1;
-// int client_fd = -1;
-
-// bool	end_sig = false;
-
-// void	handle_sigint(int sig)
-// {
-// 	(void)sig;
-// 	throw std::runtime_error("exiting...");
-// 	// end_sig = true;
-// 	// if (server_fd != -1)
-// 	// 	close(server_fd);
-// }
 
 // int main()
 // {
-// 	signal(SIGINT, handle_sigint);
-// 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-// 	sockaddr_in server_addr;
-// 	server_addr.sin_family = AF_INET;
-// 	server_addr.sin_port = htons(port);
-// 	server_addr.sin_addr.s_addr = INADDR_ANY;
-
-// 	bind(server_fd, (sockaddr *)&server_addr, sizeof(server_addr));
-
-// 	std::cout << "[" << server_fd << "]" << " listening on port: " << port << std::endl;
-// 	listen(server_fd, 5);
-
+// 	std::vector<Server*> servers;
 // 	try
 // 	{
-// 		while (!end_sig)
+// 		signal(SIGINT, handle_sigint);
+
+// 		servers.push_back(new Server("0.0.0.0", 8080));
+// 		servers.push_back(new Server("0.0.0.0", 8081));
+// 		servers.push_back(new Server("0.0.0.0", 8082));
+
+// 		std::vector<Server*>::iterator it;
+// 		for (it = servers.begin(); it != servers.end(); it++)
 // 		{
-// 			client_fd = accept(server_fd, NULL, NULL);
-// 			std::cout << "received signal by connection: [" << client_fd << "]" << std::endl;
-
-// 			std::string	buf_s;
-// 			ssize_t total_bytes = 0;
-// 			// while (1)
-// 			// {
-// 				char	buf[BUF_SIZE] = {0};
-
-// 				ssize_t b_read = recv(client_fd, buf, BUF_SIZE - 1, 0);
-// 				if (b_read <= 0)
-// 					break;
-// 				buf[b_read] = '\0';
-// 				buf_s += buf;
-// 				total_bytes += b_read;
-// 			// }
-// 			std::cout << "CONTENT: " << std::endl << buf_s << std::endl;
-// 			std::cout << "bytes read: " << total_bytes << std::endl;
-// 			char hello[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContentLength: 12\n\nHello world!";
-// 			send(client_fd, hello, strlen(hello), 0);
-// 			close(client_fd);
+// 			std::cout << "ip: " << (*(*it)).get_address() << std::endl;
+// 			std::cout << "fd: " << (*(*it)).get_socket_fd() << std::endl;
 // 		}
+// 		signal(SIGINT, SIG_DFL);
 // 	}
-
 // 	catch(const std::exception& e)
 // 	{
 // 		std::cerr << e.what() << '\n';
 // 	}
-// 	if (server_fd != -1)
-// 		close(server_fd);
+// 	std::vector<Server*>::iterator it;
+// 	for (it = servers.begin(); it != servers.end(); it++)
+// 		delete (*it);
 // }
