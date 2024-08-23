d132e22e079b8857e13fc4f99620d4892264ebc3
Okan Emre KOCA <oknakoca@gmail.com>
2024-08-23 14:12:45 +0200

    run script upgraded

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all ./webserv
+# valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all ./webserv
+if [ "$1" == "q" ]; then
+    valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all ./webserv
+else
+    valgrind --track-fds=yes --leak-check=full --show-leak-kinds=all ./webserv
+fi
